e7e79d6dc0eb7f22cdad4e708fbd3e5e0773b15e
Jim Kang <jimkang@gmail.com>
2014-02-14 20:03:27 -0500

    - Updated to add in-spotlight class when a .get is in-view. Defined in-sportlight classes, made .gets colorless if they're not what was just moved into view. - Added transitions.

--- init.js
+++ init.js
@@ -1,4 +1,17 @@
 (function init() {
+
+  NodeList.prototype.forEach = function nodeListEach(callback, thisArg) {
+    for (var i = 0; i < this.length; ++i) {
+      var value = this[i];
+      if (thisArg) {
+        callback.bind(thisArg)(value, i, this);
+      }
+      else {
+        callback(value, i, this);
+      }
+    }
+  };
+
   var scrollEmitter = createScrollEmitter();
   scrollEmitter.watchElementsWithSelectors(
     ['.get-one', '.get-two', '.get-three']
@@ -6,13 +19,18 @@
 
   document.addEventListener('elCenterMovedIntoView', 
     function onElCenterInView(e) {
+      e.detail.classList.add('in-spotlight');
+      document.querySelectorAll('.get').forEach(function removeSpotlight(el) {
+        if (el !== e.detail) {
+          el.classList.remove('in-spotlight');
+        }
+      });
+
       console.log(e.detail, '\'s center is visible.');
     }
   );
 
-  document.addEventListener('elCenterMovedOutOfView', 
-    function onElCenterOutOfView(e) {
-      console.log(e.detail, '\'s center is not visible.');
-    }
-  );
+  // Initial sync of what's in-view and the in-spotlight class.
+  scrollEmitter.respondToScroll();
+
 })();
